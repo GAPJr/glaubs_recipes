54b9f2767b34aa82d034afb4a58da688a79d9f56
Pereira Glauberio (PS/ESY2-LA) <glauberio.pereira@br.bosch.com>
2021-11-12 23:34:07 -0300

    recipe components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RootComponent } from './root/root.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeItemDetailComponent } from './recipe-list/recipe-item/recipe-item-detail/recipe-item-detail.component';
+import { ShopListComponent } from './shop-list/shop-list.component';
+import { ShopListEditComponent } from './shop-list-edit/shop-list-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [RecipeComponent, RootComponent, HeaderComponent, RecipeListComponent, RecipeItemComponent, RecipeItemDetailComponent, ShopListComponent, ShopListEditComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/recipe-list/recipe-item/recipe-item-detail/recipe-item-detail.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeItemDetailComponent } from './recipe-item-detail.component';
+
+describe('RecipeItemDetailComponent', () => {
+  let component: RecipeItemDetailComponent;
+  let fixture: ComponentFixture<RecipeItemDetailComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecipeItemDetailComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipeItemDetailComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
